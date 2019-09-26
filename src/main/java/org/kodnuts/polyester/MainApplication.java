1fe4069c964777fbd37f52b9301fc7aa0a3a49ce
Javad Sabbagh <javad.sabbaq@gmail.com>
2019-09-26 14:59:00 +0330

    Add syntax highlighter library

--- src/main/java/org/kodnuts/polyester/MainApplication.java
+++ src/main/java/org/kodnuts/polyester/MainApplication.java
@@ -5,21 +5,37 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.embed.swing.SwingNode;
+
+import javax.swing.*;
 
 public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) {
+        final SwingNode swingNode = new SwingNode();
+        createSwingContent(swingNode);
+
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+
+        Scene scene = new Scene(new StackPane(l, swingNode), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private void createSwingContent(final SwingNode swingNode) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                swingNode.setContent(new JButton("Click me!"));
+            }
+        });
     }
 
 }
---
+++ src/main/java/org/kodnuts/polyester/TextEditorDemo.java
@@ -0,0 +1,44 @@
+package org.kodnuts.polyester;
+
+import javax.swing.*;
+import org.fife.ui.rtextarea.*;
+import org.fife.ui.rsyntaxtextarea.*;
+
+import java.awt.*;
+
+/**
+ * A simple example showing how to use RSyntaxTextArea to add Java syntax
+ * highlighting to a Swing application.<p>
+ *
+ * This example uses RSyntaxTextArea 2.5.6.
+ */
+public class TextEditorDemo extends JFrame {
+
+    public TextEditorDemo() {
+
+        JPanel cp = new JPanel(new BorderLayout());
+
+        RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JSON_WITH_COMMENTS);
+        textArea.setCodeFoldingEnabled(true);
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        cp.add(sp);
+
+        setContentPane(cp);
+        setTitle("Text Editor Demo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+
+    }
+
+    public static void main(String[] args) {
+        // Start all Swing applications on the EDT.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new TextEditorDemo().setVisible(true);
+            }
+        });
+    }
+
+}
