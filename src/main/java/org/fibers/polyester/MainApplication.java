3af09c2c9d0471eb6e71489d6f6a5471504b5e3b
Javad Sabbagh <javad.sabbaq@gmail.com>
2020-02-03 18:01:54 +0330

    Fix issue: exit after closing main window

--- src/main/java/org/fibers/polyester/MainApplication.java
+++ src/main/java/org/fibers/polyester/MainApplication.java
@@ -1,11 +1,11 @@
 package org.fibers.polyester;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.embed.swing.SwingNode;
 import org.fibers.polyester.ui.LabelButton;
 
 import javax.swing.*;
@@ -22,7 +22,9 @@ public class MainApplication extends Application {
 
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Label sysLabel = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        sysLabel.setLayoutX(100);
+        sysLabel.setLayoutY(10);
 
         LabelButton l1 = new LabelButton("Result");
         l1.setLayoutX(10);
@@ -32,9 +34,15 @@ public class MainApplication extends Application {
         l2.setLayoutX(60);
         l2.setLayoutY(10);
 
-        Scene scene = new Scene(new Pane(l1, l2), 640, 480);
+        stage.setAlwaysOnTop(true); // Note: it has no effect after calling stage.show()
+
+        Scene scene = new Scene(new Pane(l1, l2, sysLabel), 640, 480);
         stage.setScene(scene);
         stage.show();
+
+        stage.setOnCloseRequest(event -> {
+            System.exit(0);
+        });
     }
 
     private SwingNode createSwingContent() {
